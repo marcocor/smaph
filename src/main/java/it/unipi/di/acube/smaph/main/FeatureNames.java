f957554673a561943bda0ac632752570e8090bb3
marcocor <cornolti@di.unipi.it>
2015-09-06 15:18:25 +0200

    added script for feature list generation

---
+++ src/main/java/it/unipi/di/acube/smaph/main/FeatureNames.java
@@ -0,0 +1,27 @@
+package it.unipi.di.acube.smaph.main;
+
+import it.unipi.di.acube.smaph.learn.featurePacks.AdvancedAnnotationFeaturePack;
+import it.unipi.di.acube.smaph.learn.featurePacks.BindingFeaturePack;
+import it.unipi.di.acube.smaph.learn.featurePacks.EntityFeaturePack;
+
+public class FeatureNames {
+
+	public static void main(String[] args) {
+		
+		String[] entityFeatures = EntityFeaturePack.ftrNames;
+		String[] annotationFeatures = AdvancedAnnotationFeaturePack.getFeatureNamesStatic();
+		String[] bindingFeatures = BindingFeaturePack.getFeatureNamesStatic();
+		System.out.println("Entity features:");
+		printFeatureNames(entityFeatures);
+		System.out.println("Annotation features:");
+		printFeatureNames(annotationFeatures);
+		System.out.println("Binding features:");
+		printFeatureNames(bindingFeatures);
+	}
+	
+	public static void printFeatureNames(String[] features){
+		for (int i=0; i<features.length; i++)
+			System.out.printf("%d %s%n", i + 1, features[i]);
+	}
+
+}
