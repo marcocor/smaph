ad513a0f4c9c6111659b40b6bf7d59f95db852d3
marcocor <cornolti@di.unipi.it>
2015-10-02 12:36:26 +0200

    updated feature selection scripts

---
+++ ranklib_feature_selection/ranking.py
@@ -0,0 +1,175 @@
+#!/bin/python
+from __future__ import print_function
+import os
+import numpy
+import os.path
+import sys
+
+RANKLIB_PATH = "../libs/RankLib-2.5.jar"
+JAVA_OPTS="-Xmx6g"
+
+def ftr_set_string(ftrs):
+	ftr_list = ""
+	i = 0
+	last_inserted = -1
+	last_block_size = 1
+	while (i < len(ftrs)):
+		current = ftrs[i]
+		if i == 0: # first feature
+			ftr_list += str(current)
+		elif current == last_inserted + 1: # continuation of a block
+			if i == len(ftrs) - 1: # last element, close block
+				ftr_list += "-" + str(current)
+			last_block_size += 1
+		else : # start of a new block
+			if last_block_size > 1:
+				ftr_list += "-" + str(last_inserted)
+			ftr_list += "," + str(current)
+			last_block_size = 1
+		last_inserted = current
+		i += 1
+	return ftr_list;
+
+assert ftr_set_string([1,2,5,6,8,10,11,12]) == "1-2,5-6,8,10-12"
+
+def parse_line(l):
+	l = l.split()
+	assert l[-2] == "#"
+	assert l[-1][:3] == "F1="
+	assert l[1][:4] == "qid:"
+	f1 = float(l[-1][3:])
+	return [(int(fv[0:fv.index(":")]), float(fv[fv.index(":")+1:])) for fv in l[2:-2]], f1, int(l[1][4:])
+
+def load_f1s(validate_file):
+	qid_cand_to_score = dict()
+	i = None
+	last_qid = -1
+	with open(validate_file) as f:
+		for l in f:
+			ftrs, f1, qid = parse_line(l)
+			if qid != last_qid:
+				last_qid = qid
+				i = 0
+			else:
+				i += 1
+			qid_cand_to_score[(qid, i)] = f1
+	return qid_cand_to_score
+
+def get_valid_ftrs(train_file):
+	diverse_features = set()
+	first_line = None
+	with open(train_file) as f:
+		for l in f:
+			if not first_line:
+				first_line = dict(parse_line(l)[0])
+			else:
+				f_dict = dict(parse_line(l)[0])
+				diverse_features |= set([f for f in f_dict.keys() if first_line[f] != f_dict[f]])
+	return sorted(list(diverse_features))
+
+def build_models(ftrs_to_try, opt_vals, ranker, train_file, validate_file, optimize="NDCG", model_name_prefix="", tree=[1000], leaf=[10]):
+	get_name = lambda optimize, t, l, op, model_name_base : '{4}.t{1}.l{2}.{0}@{3}'.format(optimize, t, l, op, model_name_base)
+	ftrs_string = ftr_set_string(ftrs_to_try)
+	features_file = ftrs_string + ".features"
+	if not os.path.isfile(features_file):
+		gen_ftr_file(ftrs_to_try)
+	model_name_base = model_name_prefix + "model_" + ftrs_string 
+	models_param = [(optimize, t, l, o, model_name_base) for t in tree for l in leaf for o in opt_vals]
+	to_train_param = [p for p in models_param if not os.path.isfile(get_name(*p))]
+	if not to_train_param:
+		print("all models already trained, skipping", file=sys.stderr)
+	else:
+		cmds = ['"java {} -jar {} -ranker {} -feature {} -train {} -validate {}'.format(JAVA_OPTS, RANKLIB_PATH, ranker, features_file, train_file, validate_file) + ' -metric2t {0}@{3} -tree {1} -leaf {2} -save {5}"'.format(*(p + (get_name(*p),))) for p in to_train_param]
+		cmd = "parallel --gnu ::: {}".format(" ".join(cmds))
+		print(cmd, file=sys.stderr)
+		os.system(cmd)
+	for p in models_param:
+		assert(os.path.isfile(get_name(*p)))
+	return [get_name(*p) for p in models_param]
+
+def get_scores(scores_file, qid_cand_to_f1):
+	qid_to_score_and_f1 = dict()
+	with open(scores_file) as f:
+		for l in f:
+			t = l.split()
+			qid, cand, p_score = int(t[0]), int(t[1]), float(t[2])
+			if qid not in qid_to_score_and_f1 or qid_to_score_and_f1[qid][0] < p_score:
+				qid_to_score_and_f1[qid] = (p_score, qid_cand_to_f1[(qid, cand)])
+	return numpy.mean(zip(*qid_to_score_and_f1.values())[1])
+
+def get_scores_f1_qid_and_maxf1(scores_file, qid_cand_to_f1):
+	qid_to_max_f1 = dict()
+	score_f1_qids = []
+	with open(scores_file) as f:
+		for l in f:
+			t = l.split()
+			qid, cand, p_score = int(t[0]), int(t[1]), float(t[2])
+			f1 = qid_cand_to_f1[(qid, cand)]
+			if qid not in qid_to_max_f1 or qid_to_max_f1[qid] < f1:
+				qid_to_max_f1[qid] = f1
+			score_f1_qids.append((p_score, f1, qid))
+	max_macro_f1 = numpy.mean(qid_to_max_f1.values())
+	return score_f1_qids, max_macro_f1, qid_to_max_f1
+
+def get_max_macrof1(qid_cand_to_f1):
+	return numpy.mean([max([qid_cand_to_f1[qid_cand] for qid_cand in qid_cand_to_f1.keys() if qid_cand[0] == qid]) for qid in set(zip(*(qid_cand_to_f1.keys()))[0])])
+
+def get_scores_binary(scores_file, qid_cand_to_f1):
+	score_f1_qids, max_macro_f1, qid_to_max_f1 = get_scores_f1_qid_and_maxf1(scores_file, qid_cand_to_f1)
+	pos_count = len([score_f1_qid for score_f1_qid in score_f1_qids if score_f1_qid[1] > 0])
+	neg_count = len([score_f1_qid for score_f1_qid in score_f1_qids if score_f1_qid[1] == 0])
+	print("positive:{} negative:{} max_f1:{}".format(pos_count, neg_count, max_macro_f1), file=sys.stderr)
+	all_scores = sorted(zip(*score_f1_qids)[0])
+	min_score = all_scores[int(len(all_scores)*0.25)] #do not range in the bottom-10% scores
+	max_score = all_scores[int(len(all_scores)*0.97)] #do not range in the top-3% scores
+	res = (-1, -1, -1, -1, -1)
+	for threshold in numpy.arange(min_score, max_score, (max_score-min_score)/50.0):
+		fn, tn, missed_best = 0, 0, 0
+		qid_to_new_best_f1 = dict()
+		for score_f1_qid in score_f1_qids:
+			if score_f1_qid[0] < threshold:
+				if score_f1_qid[1] > 0:
+					fn += 1
+				if score_f1_qid[1] == 0:
+					tn += 1
+				if score_f1_qid[1] > 0 and score_f1_qid[1] == qid_to_max_f1[score_f1_qid[2]]:
+					missed_best += 1
+			else:
+				if score_f1_qid[2] not in qid_to_new_best_f1 or qid_to_new_best_f1[score_f1_qid[2]] < score_f1_qid[1]:
+					qid_to_new_best_f1[score_f1_qid[2]] = score_f1_qid[1]
+		new_best_macro_f1 = numpy.mean(qid_to_new_best_f1.values())
+		this_res = (threshold, fn/float(neg_count), tn/float(neg_count), missed_best, new_best_macro_f1)
+		print("Thr:{} fn:{} tn:{} missed:{} new_best_f1:{}".format(*this_res), file=sys.stderr)
+
+		if (new_best_macro_f1 > max_macro_f1 - 0.03) and this_res[2] > res[2]:
+			res = this_res
+			print("Found new best.", file=sys.stderr)
+		else:
+			print("Discarding.", file=sys.stderr)
+	return res
+
+def test_models(model_names, qid_cand_to_score, validate_file, scores_computer=get_scores, scores_file_prefix=""):
+	cmd = "parallel --gnu java {} -jar {} -load {{}} -rank {} -score {}{{}}.scores ::: {}".format(JAVA_OPTS, RANKLIB_PATH, validate_file, scores_file_prefix, " ".join(model_names))
+	print(cmd, file=sys.stderr)
+	os.system(cmd)
+	return [(model_name, scores_computer(scores_file_prefix+model_name+".scores", qid_cand_to_score) if scores_computer else None) for model_name in model_names]
+
+def gen_ftr_file(ftrs):
+	filename = ftr_set_string(ftrs) + ".features"
+	with open(filename, "w") as f:
+		for ftr in ftrs:
+			f.write(str(ftr))
+			f.write("\n")
+	return filename
+
+def generate_and_test_model(ftrs_to_try, qid_cand_to_score, opt_vals, ranker, train_file, validate_file, tree=[1000], leaf=[10]):
+	print("testing feature set: {}".format(ftrs_to_try), file=sys.stderr)
+	gen_ftr_file(ftrs_to_try)
+	models = build_models(ftrs_to_try, opt_vals, ranker, train_file, validate_file, tree=tree, leaf=leaf)
+	print("generated models: {}".format(" ".join(models)), file=sys.stderr)
+	models_and_f1s = test_models(models, qid_cand_to_score, validate_file)
+	for p in models_and_f1s:
+		print("model {} F1={}".format(*p), file=sys.stderr)
+	model, f1 = max(models_and_f1s, key=lambda p: p[1])
+	return model, f1
+
--- ranklib_feature_selection/ranklib_feature_selection.py
+++ ranklib_feature_selection/ranklib_feature_selection.py
@@ -4,148 +4,79 @@ import os
 import numpy
 import os.path
 import sys
+from ranking import *
 
 RANKLIB_PATH = "../libs/RankLib-2.5.jar"
 RANKER = 6
 TRAIN_DATA = "../train_binding_ranking.dat"
 VALIDATE_DATA = "../devel_binding_ranking.dat"
-OPT_VALS = [9,11,13,15,17,19,21,23]
+#TRAIN_DATA = "train_bin_stage2.dat"
+#VALIDATE_DATA = "devel_bin_stage2.dat"
+OPT_VALS = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 JAVA_OPTS="-Xmx6g"
 
-def ftr_set_string(ftrs):
-	ftr_list = ""
-	i = 0
-	last_inserted = -1
-	last_block_size = 1
-	while (i < len(ftrs)):
-		current = ftrs[i]
-		if i == 0: # first feature
-			ftr_list += str(current)
-		elif current == last_inserted + 1: # continuation of a block
-			if i == len(ftrs) - 1: # last element, close block
-				ftr_list += "-" + str(current)
-			last_block_size += 1
-		else : # start of a new block
-			if last_block_size > 1:
-				ftr_list += "-" + str(last_inserted)
-			ftr_list += "," + str(current)
-			last_block_size = 1
-		last_inserted = current
-		i += 1
-	return ftr_list;
+def ftr_selection_loop(good_ftr, ftr_buckets_left, qid_cand_to_score, best_f1, update_foo):
+	print("all feature buckets to try: {}".format(ftr_buckets_left), file=sys.stderr)
+	while True:
+		best_in_iteration = None
+		print("starting iteration with feature set: {}".format(good_ftr), file=sys.stderr)
+		for feature_bucket in ftr_buckets_left:
+			ftrs_to_try = update_foo(good_ftr, feature_bucket)
+			model, f1 = generate_and_test_model(ftrs_to_try, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA)
+			print("achieved f1={} with model {} and feature set {} [added/removed features {}]".format(f1, model, ftrs_to_try, feature_bucket), file=sys.stderr)
+			if not best_in_iteration or f1 >= best_in_iteration[1]:
+				print("found new best score inside iteration. F1={} ({}overall best)".format(f1, "no " if f1 <= best_f1 else ""), file=sys.stderr)
+				best_in_iteration = (feature_bucket, f1)
+		if not best_in_iteration or best_in_iteration[1] < best_f1:
+			print("no advances in this iteration, stopping iterations.", file=sys.stderr)
+			break
+		else:
+			best_f1 = best_in_iteration[1]
+			good_ftr = update_foo(good_ftr, best_in_iteration[0])
+			ftr_buckets_left.remove(best_in_iteration[0])
+			print("iteration finished: found best F1={} selected feature {}".format(best_in_iteration[1], best_in_iteration[0]), file=sys.stderr)
+		if not ftr_buckets_left:
+			print("no more features to try, stopping interations.", file=sys.stderr)
+			break
+	return good_ftr, best_f1
 
-assert ftr_set_string([1,2,5,6,8,10,11,12]) == "1-2,5-6,8,10-12"
+increment_features = lambda prev_ftrs, ftr_bucket: sorted(prev_ftrs + ftr_bucket)
+decrement_features = lambda prev_ftrs, ftr_bucket: sorted([f for f in prev_ftrs if f not in ftr_bucket])
 
-def parse_line(l):
-	l = l.split()
-	assert l[-2] == "#"
-	assert l[-1][:3] == "F1="
-	assert l[1][:4] == "qid:"
-	f1 = float(l[-1][3:])
-	return [(int(fv[0:fv.index(":")]), float(fv[fv.index(":")+1:])) for fv in l[2:-2]], f1, int(l[1][4:])
+FTR_STEP = 1
+good_ftr = [] #[2, 3, 4, 151, 154, 155, 156, 162, 169, 170, 171, 172, 173, 174, 199, 200, 201, 202, 203, 204, 220, 221, 222, 223, 224, 225]
 
-def load_validate_f1s():
-	qid_cand_to_score = dict()
-	i = None
-	last_qid = -1
-	with open(VALIDATE_DATA) as f:
-		for l in f:
-			ftrs, f1, qid = parse_line(l)
-			if qid != last_qid:
-				last_qid = qid
-				i = 0
-			else:
-				i += 1
-			qid_cand_to_score[(qid, i)] = f1
-	return qid_cand_to_score
+all_ftrs = list(reversed(get_valid_ftrs(VALIDATE_DATA)))
+ftr_buckets_left = []
+for r in range(0, len(all_ftrs), FTR_STEP):
+	bucket = sorted([f for f in all_ftrs[r:r+FTR_STEP] if f not in good_ftr])
+	if bucket:
+		ftr_buckets_left.append(bucket)
+ftr_buckets_left = sorted(ftr_buckets_left)
 
-def get_valid_ftrs():
-	diverse_features = set()
-	first_line = None
-	with open(TRAIN_DATA) as f:
-		for l in f:
-			if not first_line:
-				first_line = dict(parse_line(l)[0])
-			else:
-				f_dict = dict(parse_line(l)[0])
-				diverse_features |= set([f for f in f_dict.keys() if first_line[f] != f_dict[f]])
-	return sorted(list(diverse_features))
+qid_cand_to_score = load_f1s(VALIDATE_DATA)
 
-def build_models(ftrs_to_try):
-	ftrs_string = ftr_set_string(ftrs_to_try)
-	model_name_base = "model_" + ftrs_string 
-	to_train = [o for o in OPT_VALS if not os.path.isfile("{}.NDCG@{}".format(model_name_base, o))]
-	if not to_train:
-		print("all models already trained, skipping", file=sys.stderr)
-	else:
-		cmd = "parallel --gnu java {} -jar {} -ranker {} -feature {}.features -metric2t NDCG@{{}} -train {} -validate {} -save {}.NDCG@{{}} ::: {}".format(JAVA_OPTS, RANKLIB_PATH, RANKER, ftrs_string, TRAIN_DATA, VALIDATE_DATA, model_name_base, " ".join(str(o) for o in to_train))
-		print(cmd, file=sys.stderr)
-		os.system(cmd)
-	res= ["{}.NDCG@{}".format(model_name_base, o) for o in OPT_VALS]
-	for filename in res:
-		assert(os.path.isfile(filename))
-	return res
 
-def get_scores(scores_file, qid_cand_to_f1):
-	qid_to_score_and_f1 = dict()
-	with open(scores_file) as f:
-		for l in f:
-			t = l.split()
-			qid, cand, p_score = int(t[0]), int(t[1]), float(t[2])
-			if qid not in qid_to_score_and_f1 or qid_to_score_and_f1[qid][0] < p_score:
-				qid_to_score_and_f1[qid] = (p_score, qid_cand_to_f1[(qid, cand)])
-	assert set(p[0] for p in qid_cand_to_score.keys()) == set(qid_to_score_and_f1.keys())
-	return numpy.mean(zip(*qid_to_score_and_f1.values())[1])
-				
+if good_ftr:
+	print("Initialization - testing initial feature set", file=sys.stderr)
+	_, best_f1 = generate_and_test_model(good_ftr, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA)
+	print("Initial best score: {}".format(best_f1), file=sys.stderr)
 
-def test_models(model_names, qid_cand_to_score):
-	cmd = "parallel --gnu java {} -jar {} -load {{}} -rank {} -score {{}}.scores ::: {}".format(JAVA_OPTS, RANKLIB_PATH, VALIDATE_DATA, " ".join(model_names))
-	print(cmd, file=sys.stderr)
-	os.system(cmd)
-	return [(model_name, get_scores(model_name+".scores", qid_cand_to_score)) for model_name in model_names]
+print("First phase - feature increment", file=sys.stderr)
+good_ftr, best_f1 = ftr_selection_loop(good_ftr, ftr_buckets_left, qid_cand_to_score, best_f1, increment_features)
+print("1st phase - Overall best features: {}".format(good_ftr), file=sys.stderr)
+print("1st phase - Overall best score: {}".format(best_f1), file=sys.stderr)
 
-def gen_ftr_file(ftrs):
-	filename = ftr_set_string(ftrs_to_try) + ".features"
-	with open(filename, "w") as f:
-		for ftr in ftrs:
-			f.write(str(ftr))
-			f.write("\n")
+print("Second phase - feature ablation", file=sys.stderr)
+good_ftr, best_f1 = ftr_selection_loop(good_ftr, [[f] for f in good_ftr], qid_cand_to_score, best_f1, decrement_features)
+print("2nd phase - Overall best features: {}".format(good_ftr), file=sys.stderr)
+print("2nd phase - Overall best score: {}".format(best_f1), file=sys.stderr)
 
-good_ftr = []
-ftr_buckets_left = (lambda fl : sorted([sorted(fl[r:r+3]) for r in range(0, len(fl), 3)])) (list(reversed(get_valid_ftrs())))
-print("all feature buckets: {}".format(ftr_buckets_left), file=sys.stderr)
-qid_cand_to_score = load_validate_f1s()
-best_f1 = None
+print("Third phase - Parameter tuning", file=sys.stderr)
+for t in [200, 500,1000]:
+	for l in [7,8,9,10,11,12]:
+		model, best_f1 = generate_and_test_model(good_ftr, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA, tree=[t], leaf=[l])
+		print("3rd phase - Overall best model: {}".format(model), file=sys.stderr)
+		print("3rd phase - Overall best score: {}".format(best_f1), file=sys.stderr)
+		
 
-while True:
-	best_in_iteration = None
-	print("starting iteration with feature set: {}".format(good_ftr), file=sys.stderr)
-	for feature_bucket in ftr_buckets_left:
-		ftrs_to_try = sorted(good_ftr + feature_bucket)
-		print("testing feature set: {}".format(ftrs_to_try), file=sys.stderr)
-		gen_ftr_file(ftrs_to_try)
-		models = build_models(ftrs_to_try)
-		print("generated models: {}".format(" ".join(models)), file=sys.stderr)
-		models_and_f1s = test_models(models, qid_cand_to_score)
-		for p in models_and_f1s:
-			print("model {} F1={}".format(*p), file=sys.stderr)
-		model, f1 = max(models_and_f1s, key=lambda p: p[1])
-		print("achieved f1={} with model {} and feature set {}".format(f1, model, ftrs_to_try), file=sys.stderr)
-	
-		if not best_in_iteration or f1 >= best_in_iteration[1]:
-			print("found new best score inside iteration. F1={}".format(f1), file=sys.stderr)
-			best_in_iteration = (feature_bucket, f1)
-	if not best_in_iteration or best_in_iteration[1] < best_f1:
-		print("no advances in this iteration, stopping iterations.", file=sys.stderr)
-		break
-	else:
-		best_f1 = best_in_iteration
-		good_ftr = sorted(good_ftr + best_in_iteration[0])
-		ftr_buckets_left.remove(best_in_iteration[0])
-		print("iteration finished: found best F1={} adding feature {}".format(best_in_iteration[1], best_in_iteration[0]), file=sys.stderr)
-	if not ftr_buckets_left:
-		print("no more features to try, stopping interations.", file=sys.stderr)
-		break
-
-print("Overall best features: {}".format(good_ftr), file=sys.stderr)
-print("Overall best score: {}".format(best_f1), file=sys.stderr)
