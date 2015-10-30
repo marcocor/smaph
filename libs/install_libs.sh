d5dbae120702bdc935fdde94146d01588481719f
marcocor <cornolti@di.unipi.it>
2015-10-30 15:55:33 +0100

    added/updated libs; updated scripts

--- libs/install_libs.sh
+++ libs/install_libs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-mvn install:install-file -Dfile=RankLib-2.1-patched.jar -DgroupId=ranklib.plus -DartifactId=ranklib -Dversion=2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=RankLib-2.5.jar -DgroupId=ranklib.plus -DartifactId=ranklib -Dversion=2.5 -Dpackaging=jar
 mvn install:install-file -Dfile=hpc-utils-0.0.5.jar -DgroupId=it.cnr.isti.hpc -DartifactId=hpc-utils -Dversion=0.0.5 -Dpackaging=jar
+
+unzip liblinear-2.0.zip
+pushd liblinear-2.0
+make
+popd
