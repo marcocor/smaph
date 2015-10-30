29208de35ebaba7d00bd6d1663001d1a80486d25
marcocor <cornolti@di.unipi.it>
2015-10-30 17:27:21 +0100

    implemented vertical pruning

--- ranklib_feature_selection/ranklib_feature_selection.py
+++ ranklib_feature_selection/ranklib_feature_selection.py
@@ -5,78 +5,118 @@ import numpy
 import os.path
 import sys
 from ranking import *
+import argparse
 
 RANKLIB_PATH = "../libs/RankLib-2.5.jar"
 RANKER = 6
-TRAIN_DATA = "../train_binding_ranking.dat"
-VALIDATE_DATA = "../devel_binding_ranking.dat"
-#TRAIN_DATA = "train_bin_stage2.dat"
-#VALIDATE_DATA = "devel_bin_stage2.dat"
-OPT_VALS = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
-JAVA_OPTS="-Xmx6g"
 
 def ftr_selection_loop(good_ftr, ftr_buckets_left, qid_cand_to_score, best_f1, update_foo):
 	print("all feature buckets to try: {}".format(ftr_buckets_left), file=sys.stderr)
 	while True:
-		best_in_iteration = None
+		better_alternatives = []
 		print("starting iteration with feature set: {}".format(good_ftr), file=sys.stderr)
 		for feature_bucket in ftr_buckets_left:
 			ftrs_to_try = update_foo(good_ftr, feature_bucket)
 			model, f1 = generate_and_test_model(ftrs_to_try, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA)
 			print("achieved f1={} with model {} and feature set {} [added/removed features {}]".format(f1, model, ftrs_to_try, feature_bucket), file=sys.stderr)
-			if not best_in_iteration or f1 >= best_in_iteration[1]:
-				print("found new best score inside iteration. F1={} ({}overall best)".format(f1, "no " if f1 <= best_f1 else ""), file=sys.stderr)
-				best_in_iteration = (feature_bucket, f1)
-		if not best_in_iteration or best_in_iteration[1] < best_f1:
+			if is_improvement(f1, ftrs_to_try, best_f1, good_ftr):
+				print("found a better score inside iteration. F1={}".format(f1), file=sys.stderr)
+				better_alternatives = sorted([(f1, feature_bucket)] + better_alternatives, reverse=True)
+		if not better_alternatives:
 			print("no advances in this iteration, stopping iterations.", file=sys.stderr)
 			break
 		else:
-			best_f1 = best_in_iteration[1]
-			good_ftr = update_foo(good_ftr, best_in_iteration[0])
-			ftr_buckets_left.remove(best_in_iteration[0])
-			print("iteration finished: found best F1={} selected feature {}".format(best_in_iteration[1], best_in_iteration[0]), file=sys.stderr)
+			print("starting vertical pruning. order:{}".format(better_alternatives), file=sys.stderr)
+			for feature_bucket in zip(*better_alternatives)[1]:
+				print("trying to add/remove {}".format(feature_bucket), file=sys.stderr)
+				ftrs_to_try = update_foo(good_ftr, feature_bucket)
+				model, f1 = generate_and_test_model(ftrs_to_try, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA)
+				if is_improvement(f1, ftrs_to_try, best_f1, good_ftr):
+					best_f1 = f1
+					good_ftr = ftrs_to_try
+					ftr_buckets_left.remove(feature_bucket)
+					print("vertical pruning: found best F1={} selected feature {}".format(best_f1, feature_bucket), file=sys.stderr)
+				else:
+					print("vertical pruning: no new best found with features {} (F1={})".format(feature_bucket, best_f1), file=sys.stderr)
 		if not ftr_buckets_left:
 			print("no more features to try, stopping interations.", file=sys.stderr)
 			break
 	return good_ftr, best_f1
 
-increment_features = lambda prev_ftrs, ftr_bucket: sorted(prev_ftrs + ftr_bucket)
-decrement_features = lambda prev_ftrs, ftr_bucket: sorted([f for f in prev_ftrs if f not in ftr_bucket])
+def get_feature_buckets(all_ftrs, step):
+	ftr_buckets_left = []
+	for r in range(0, len(all_ftrs), step):
+		bucket = sorted(all_ftrs[r:r+step])
+		if bucket:
+			ftr_buckets_left.append(bucket)
+	return sorted(ftr_buckets_left)
 
-FTR_STEP = 1
-good_ftr = [] #[2, 3, 4, 151, 154, 155, 156, 162, 169, 170, 171, 172, 173, 174, 199, 200, 201, 202, 203, 204, 220, 221, 222, 223, 224, 225]
+def do_one_phase(update_foo, good_ftr, valid_ftrs, best_f1, step):
+	candidate_ftrs = [f for f in valid_ftrs if f not in good_ftr] if update_foo == increment_features else good_ftr
+	ftr_buckets_left = get_feature_buckets(candidate_ftrs, step)
+	print("Stage: {} - {} by {}".format(update_foo.__name__, step, step), file=sys.stderr)
+	good_ftr, best_f1 = ftr_selection_loop(good_ftr, ftr_buckets_left, qid_cand_to_score, best_f1, update_foo)
+	print("Overall best features: {}".format(good_ftr), file=sys.stderr)
+	print("Overall best score: {}".format(best_f1), file=sys.stderr)
+	
+	return good_ftr, best_f1
 
-all_ftrs = list(reversed(get_valid_ftrs(VALIDATE_DATA)))
-ftr_buckets_left = []
-for r in range(0, len(all_ftrs), FTR_STEP):
-	bucket = sorted([f for f in all_ftrs[r:r+FTR_STEP] if f not in good_ftr])
-	if bucket:
-		ftr_buckets_left.append(bucket)
-ftr_buckets_left = sorted(ftr_buckets_left)
+def increment_features(prev_ftrs, ftr_bucket):
+	return sorted(prev_ftrs + ftr_bucket)
 
-qid_cand_to_score = load_f1s(VALIDATE_DATA)
+def decrement_features(prev_ftrs, ftr_bucket):
+	return sorted([f for f in prev_ftrs if f not in ftr_bucket])
+
+def is_improvement(new_f1, new_ftrs, old_f1, old_ftrs):
+	if len(new_ftrs) < len(old_ftrs):
+		return new_f1 >= old_f1 #with a smaller feature set, can we achieve the same (or better) score?
+	else:
+		return new_f1 > old_f1 #with a bigger feature set, can we achieve a strictly better score?
 
 
-if good_ftr:
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Do feature selections')
+	parser.add_argument('--method', help="Can be ablation or increment", choices=['ablation', 'increment'], required=True)
+	parser.add_argument('--dataset', help="Dataset code (e.g. ERD-S2S3S6)", required=True)
+	parser.add_argument('--startset', help="Feature set to start with (e.g. 1,3,5-10,24)", required=False, default="")
+	args = parser.parse_args()
+
+	good_ftr = ftr_string_set(args.startset)
+
+	OPT_VALS = range(12,17+1)
+	TRAIN_DATA = "../train_binding_ranking_{}.dat".format(args.dataset)
+	VALIDATE_DATA = "../devel_binding_ranking_{}.dat".format(args.dataset)
+	
+	print("Loading solutions F1...", file=sys.stderr)
+	qid_cand_to_score = load_f1s(VALIDATE_DATA)
+	
+	print("Getting valid features...", file=sys.stderr)
+	valid_ftrs = get_valid_ftrs(VALIDATE_DATA)
+	
+	if args.method == 'ablation':
+		main_method, secondary_method = decrement_features, increment_features
+	elif args.method == 'increment':
+		main_method, secondary_method = increment_features, decrement_features
+	
 	print("Initialization - testing initial feature set", file=sys.stderr)
-	_, best_f1 = generate_and_test_model(good_ftr, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA)
+	_, best_f1 = generate_and_test_model(good_ftr if good_ftr else valid_ftrs, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA)
 	print("Initial best score: {}".format(best_f1), file=sys.stderr)
+	
+	if args.method == 'increment' and not good_ftr: #start from scratch
+		best_f1 = 0.0
 
-print("First phase - feature increment", file=sys.stderr)
-good_ftr, best_f1 = ftr_selection_loop(good_ftr, ftr_buckets_left, qid_cand_to_score, best_f1, increment_features)
-print("1st phase - Overall best features: {}".format(good_ftr), file=sys.stderr)
-print("1st phase - Overall best score: {}".format(best_f1), file=sys.stderr)
+	good_ftr, best_f1 = do_one_phase(main_method, good_ftr, valid_ftrs, best_f1, 6)
+	good_ftr, best_f1 = do_one_phase(main_method, good_ftr, valid_ftrs, best_f1, 3)
+	good_ftr, best_f1 = do_one_phase(main_method, good_ftr, valid_ftrs, best_f1, 2)
+	good_ftr, best_f1 = do_one_phase(main_method, good_ftr, valid_ftrs, best_f1, 1)
+	
+	good_ftr, best_f1 = do_one_phase(secondary_method, good_ftr, valid_ftrs, best_f1, 1)
 
-print("Second phase - feature ablation", file=sys.stderr)
-good_ftr, best_f1 = ftr_selection_loop(good_ftr, [[f] for f in good_ftr], qid_cand_to_score, best_f1, decrement_features)
-print("2nd phase - Overall best features: {}".format(good_ftr), file=sys.stderr)
-print("2nd phase - Overall best score: {}".format(best_f1), file=sys.stderr)
-
-print("Third phase - Parameter tuning", file=sys.stderr)
-for t in [200, 500,1000]:
-	for l in [7,8,9,10,11,12]:
-		model, best_f1 = generate_and_test_model(good_ftr, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA, tree=[t], leaf=[l])
-		print("3rd phase - Overall best model: {}".format(model), file=sys.stderr)
-		print("3rd phase - Overall best score: {}".format(best_f1), file=sys.stderr)
-		
-
+	print("Parameter tuning", file=sys.stderr)
+	for t in [200, 500, 1000]:
+		for l in [7,8,9,10,11,12]:
+			model, best_f1 = generate_and_test_model(good_ftr, qid_cand_to_score, OPT_VALS, RANKER, TRAIN_DATA, VALIDATE_DATA, tree=[t], leaf=[l])
+			print("3rd phase - Overall best model: {}".format(model), file=sys.stderr)
+			print("3rd phase - Overall best score: {}".format(best_f1), file=sys.stderr)
+			
+	
