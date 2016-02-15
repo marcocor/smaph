13f5f4c370ff835c4e183f79270042c86cc2fe61
marcocor <cornolti@di.unipi.it>
2016-02-15 12:57:49 +0100

    added logger

--- src/main/java/it/unipi/di/acube/smaph/server/ServerMain.java
+++ src/main/java/it/unipi/di/acube/smaph/server/ServerMain.java
@@ -3,12 +3,18 @@ package it.unipi.di.acube.smaph.server;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import it.unipi.di.acube.smaph.server.rest.RestService;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.net.URI;
 
 public class ServerMain {
 	public static final String BASE_URI = "http://localhost:8080/smaph/";
+	private final static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	/**
 	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
@@ -29,8 +35,10 @@ public class ServerMain {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
+		LOG.info("Initializing server.");
+		RestService.initialize();
 		HttpServer server = startServer();
-		System.out.println(String.format("Smaph started with WADL available at " + "%sapplication.wadl%nPress Enter to terminate.", BASE_URI));
+		LOG.info(String.format("Smaph started with WADL available at " + "%sapplication.wadl%nPress Enter to terminate.", BASE_URI));
 		System.in.read();
 		server.shutdown();
 	}
