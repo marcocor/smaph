1d98b8f672d9fa73e6f6adcefc8bf70526ecbbb4
marco <marco@macor.di.unipi.it>
2016-11-28 18:08:48 +0100

    Updated default top-k parameters.

--- src/main/java/it/unipi/di/acube/smaph/SmaphBuilder.java
+++ src/main/java/it/unipi/di/acube/smaph/SmaphBuilder.java
@@ -34,9 +34,9 @@ public class SmaphBuilder {
 	        "COMMONNESS", "mw", "0.2", "0.0");
 	public static WebsearchApi BING_WEBSEARCH_API = null;
 	public static WebsearchApi GOOGLE_WEBSEARCH_API = null;
+	public static final int DEFAULT_NORMALSEARCH_RESULTS = 5;
 	public static final int DEFAULT_WIKISEARCH_RESULTS = 10;
-	public static final int DEFAULT_ANNOTATED_SNIPPETS = 25;
-	public static final int DEFAULT_NORMALSEARCH_RESULTS = 25;
+	public static final int DEFAULT_ANNOTATED_SNIPPETS = 15;
 	public static final double DEFAULT_ANNOTATIONFILTER_RATIO = 0.03;
 	public static final double DEFAULT_ANCHOR_MENTION_ED = 0.7;
 
