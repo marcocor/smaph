85eabe7f5b1ea5ad7bce60c3147e6f937ae6ee78
marco <marco@macor.di.unipi.it>
2017-05-04 17:39:16 +0200

    Added greedy features names

--- src/main/java/it/unipi/di/acube/smaph/main/utils/FeatureNames.java
+++ src/main/java/it/unipi/di/acube/smaph/main/utils/FeatureNames.java
@@ -3,6 +3,7 @@ package it.unipi.di.acube.smaph.main.utils;
 import it.unipi.di.acube.smaph.learn.featurePacks.AnnotationFeaturePack;
 import it.unipi.di.acube.smaph.learn.featurePacks.BindingFeaturePack;
 import it.unipi.di.acube.smaph.learn.featurePacks.EntityFeaturePack;
+import it.unipi.di.acube.smaph.learn.featurePacks.GreedyFeaturePack;
 
 public class FeatureNames {
 
@@ -11,12 +12,15 @@ public class FeatureNames {
 		String[] entityFeatures = EntityFeaturePack.ftrNames;
 		String[] annotationFeatures = AnnotationFeaturePack.getFeatureNamesStatic();
 		String[] bindingFeatures = BindingFeaturePack.getFeatureNamesStatic();
+		String[] greedyFeatures = GreedyFeaturePack.getFeatureNamesStatic();
 		System.out.println("Entity features:");
 		printFeatureNames(entityFeatures);
 		System.out.println("Annotation features:");
 		printFeatureNames(annotationFeatures);
 		System.out.println("Binding features:");
 		printFeatureNames(bindingFeatures);
+		System.out.println("Greedy features:");
+		printFeatureNames(greedyFeatures);
 	}
 	
 	public static void printFeatureNames(String[] features){
